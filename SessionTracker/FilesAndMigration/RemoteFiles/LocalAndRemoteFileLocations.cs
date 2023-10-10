15d0741ba05e95bdb301d6dff831a2151b028a17
Taschenbuch <43114787+Taschenbuch@users.noreply.github.com>
2023-10-11 00:13:47 +0200

    use constants in FileConstants

--- SessionTracker/FilesAndMigration/RemoteFiles/LocalAndRemoteFileLocations.cs
+++ SessionTracker/FilesAndMigration/RemoteFiles/LocalAndRemoteFileLocations.cs
@@ -8,7 +8,7 @@ namespace SessionTracker.Files.RemoteFiles
         public LocalAndRemoteFileLocations(FileConstants fileConstants, DirectoriesManager directoriesManager)
         {
             _fileConstants = fileConstants;
-            LocalRootFolderPath = directoriesManager.GetFullDirectoryPath(fileConstants.ModuleFolderName);
+            LocalRootFolderPath = directoriesManager.GetFullDirectoryPath(FileConstants.MODULE_FOLDER_NAME);
             CreateFileLocations();
         }
 
@@ -41,10 +41,10 @@ namespace SessionTracker.Files.RemoteFiles
 
         private string GetRemoteFilePath(string fileName)
         {
-            return $"{_fileConstants.RemoteBaseUrl}/{_relativeFolderPath}/{fileName}";
+            return $"{FileConstants.REMOTE_BASE_URL}/{_relativeFolderPath}/{fileName}";
         }
 
-        private string _relativeFolderPath => $"data/format_version_{_fileConstants.FormatVersion}";
+        private string _relativeFolderPath => $"data/format_version_{FileConstants.FORMAT_VERSION}";
         private readonly FileConstants _fileConstants;
     }
 }
