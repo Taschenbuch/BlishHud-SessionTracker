832023c83c35ba16fc7ca26f43f14d75cfe8e926
Taschenbuch <43114787+Taschenbuch@users.noreply.github.com>
2023-10-13 23:35:52 +0200

    fix: stat window forgot stored position after blish startup and moved against a gw2 screen border instead
    * was caused by adjusting stats into screen boundaries inside RecalculateLayout(). Because of the multiple spriteScreen resizes on blish startup, this caused an adjustment of the position. May not happen on every system (timing issue)
    * and fix relative coordinate was stored as absolute max screen height/width when outside of bottom/right of gw2 screen. Caused stats window to be out of reach. bug is very old but was triggered by new window anchor feature for the first time.
    * and fix for stat windows which do not fit into gw2 screen boundaries that the anchor can be outside of gw2 boundaries and then out of reach when all stats are hidden

---
+++ SessionTracker/RelativePositionWindow/WindowAnchorService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using SessionTracker.SettingEntries;
+
+namespace SessionTracker.RelativePositionWindow
+{
+    public class WindowAnchorService
+    {
+        public static Point ConvertBetweenControlAndWindowAnchorLocation(Point location, Point controlSize, ConvertLocation convertLocation, WindowAnchor windowAnchor)
+        {
+            var xIsControlLocation = windowAnchor == WindowAnchor.TopLeft || windowAnchor == WindowAnchor.BottomLeft;
+            var yIsControlLocation = windowAnchor == WindowAnchor.TopLeft || windowAnchor == WindowAnchor.TopRight;
+            var x = ConvertCoordinate(location.X, controlSize.X, xIsControlLocation, convertLocation);
+            var y = ConvertCoordinate(location.Y, controlSize.Y, yIsControlLocation, convertLocation);
+            return new Point(x, y);
+        }
+
+        private static int ConvertCoordinate(int coordinate, int controlWidthOrHeight, bool isControlLocation, ConvertLocation convertLocation)
+        {
+            if (isControlLocation)
+                return coordinate;
+
+            return convertLocation == ConvertLocation.ToWindowAnchorLocation
+                ? coordinate + controlWidthOrHeight
+                : coordinate - controlWidthOrHeight;
+        }
+    }
+}
